3521d033b13c3f4e0466e00c5f0f578ba87af8c1
Luke Amdor <lamdor@geolearning.com>
2010-11-18 10:31:36 -0600

    Updated winetricks to newest

--- Library/Formula/winetricks.rb
+++ Library/Formula/winetricks.rb
@@ -3,14 +3,14 @@ require 'formula'
 class Winetricks < ScriptFileFormula
   url 'http://www.kegel.com/wine/winetricks', :using => :curl
   homepage 'http://wiki.winehq.org/winetricks'
-  version '20101008'
+  version '20101106'
 
   head 'http://winezeug.googlecode.com/svn/trunk/winetricks', :using => :curl
 
   if ARGV.build_head?
     md5 'c42ee409c7c4b71dfd4ee326aab8e14c'
   else
-    md5 '2953c076ab699a659d06a4790a2ab602'
+    md5 '912b829ec55c0e3634f1577a67ce3277'
   end
 
   def caveats; <<-EOS.undent
